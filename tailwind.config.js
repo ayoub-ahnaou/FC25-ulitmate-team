2ef775e72db626dd06ef9a6062c4ca5873477128
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-25 14:28:05 +0100

    DEfinr the color palette in configuration file and input css file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        greenColor: "#0B3D0B",
+        darkGray: "#3B3B3B",
+        softGray: "#f5f5f5",
+        goldColor: "#B8860B",
+        mainBlack: "#1A1A1A",
+      }
+    },
   },
   plugins: [],
 }
