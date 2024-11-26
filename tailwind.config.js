1788b8f7ee34f3b5c9cb24073fa9f37e51d7ff7f
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-26 22:28:03 +0100

    #16 Custome the stats color to use in players statistique

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,13 @@ module.exports = {
         softGray: "#f5f5f5",
         goldColor: "#B8860B",
         mainBlack: "#1A1A1A",
+
+        veryPoor: "#A9A9A9", // 0 - 20
+        poor: "#FF6347", // 21 - 40
+        average: "#FFD700", // 41 - 60
+        aboveAverage: "#FFA500", // 61 - 80
+        good: "#32CD32", // 81 - 90
+        excellent: "#16FF05", // 91 - 100
       },
       fontFamily: {
         gaMaamli: ["Ga Maamli", "sans-serif"],
