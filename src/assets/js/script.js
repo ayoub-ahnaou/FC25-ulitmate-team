328a12bd69196291465b14bd118520d180cbbaf7
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-26 22:35:27 +0100

    #16 Call the function that will send the ID of the player to follow specifically

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,7 +4,7 @@ const playersList = document.getElementById("players-list");
 
 players.map((player) => {
     playersList.innerHTML += `
-        <div class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
+        <div onclick="showPlayerDetails('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
             <img class="h-full" src=${player.photo} alt=${player.name}>
             <div class="flex flex-col h-full text-[10px] justify-center">
                 <span>${player.name}</span>
