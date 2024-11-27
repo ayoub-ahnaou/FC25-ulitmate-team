bdd867483c9e005c0150221601244d28ff51704b
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-27 16:01:17 +0100

    #9 Create a function to list players based on the filtered players arrays

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -68,9 +68,32 @@ document.addEventListener("click", (e) => {
 })
 
 const goalkepeers = players.filter((gk) => gk.position === "GK")
-const centralMidfielders = players.filter((cm) => cm.position === "CM")
 const deffencers = players.filter((cb) => cb.position === "CB" || cb.position === "RB" || cb.position === "LB")
+const midfielders = players.filter((cm) => cm.position === "CM")
 const attackers = players.filter((ac) => ac.position === "RW" || ac.position === "LW" || ac.position === "ST")
 
 const starters = []; // array where the 11 players will be stored
 const bench = []; // array where changment will be stored
+
+// function to update list players with possible players based on the position
+function updateListPlayers(array) {
+    const playersList = document.getElementById("players-list");
+
+    playersList.innerHTML = "";
+    array.map((player) => {
+        playersList.innerHTML += `
+            <div onclick="showPlayerDetails('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
+                <img class="h-full" src=${player.photo} alt=${player.name}>
+                <div class="flex flex-col h-full text-[10px] justify-center">
+                    <span>${player.name}</span>
+                    <span class="text-goldColor">${player.position}</span>
+                </div>
+                <div class="flex-1 h-full flex items-center gap-2 justify-end">
+                    <img src=${player.logo} class="h-1/2" alt="">
+                    <img src=${player.flag} class="h-1/2" alt="">
+                    <span class="text-goldColor text-lg">${player.rating}</span>
+                </div>
+            </div>
+        `;
+    });
+}
