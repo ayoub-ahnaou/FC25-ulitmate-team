5e2679e2757a51d95c03da46c60841e1b24fa8b4
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-11-28 13:26:49 +0100

    #9 Fix issue club not showing image, also declare variables that will store the whole team for localStorage

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -14,7 +14,7 @@ window.showPlayerDetails = (player_id) => {
     playerDetailsContainer.classList.remove("hidden")
     playerDetailsContainer.classList.add("fixed");
     playerDetailsContainer.innerHTML = `
-        <div class="flex w-[500px] h-[400px] max-md:h-auto rounded-md overflow-hidden max-md:flex-col" id="player-details">
+        <div class="flex w-[400px] h-[400px] max-md:h-auto rounded-md overflow-hidden max-md:flex-col relative" id="player-details">
             <div class="max-md:w-full w-2/3 relative">
                 <img src=${player.large_pic} class="h-full max-md:h-[350px] w-full object-cover" alt="">
                 <span class="absolute top-0 p-8 text-6xl drop-shadow-[5px_5px_20px_rgba(255,255,255,0.99)] text-goldColor">${player.rating}</span>
@@ -23,16 +23,15 @@ window.showPlayerDetails = (player_id) => {
                     <div class="flex justify-between items-center h-full">
                         <span class="text-xl text-goldColor">${player.position}</span>
                         <div class="flex gap-2 h-full">
-                            <img src=${player.club} class="h-10" alt="">
                             <img src=${player.logo} class="h-10" alt="">
+                            <img src=${player.flag} class="h-10" alt="">
                         </div>
                     </div>
                 </div>
             </div>
-            <div class="bg-darkGray w-1/3 h-full max-md:w-full flex flex-col p-4 gap-2 max-md:gap-6 justify-between">
+            <div class="bg-darkGray w-1/3 h-full max-md:w-full flex flex-col p-4 gap-2 max-md:gap-4 justify-between">
                 <div class="flex flex-col gap-2" id="stats-details">    </div>
-                <div class="h-10 max-md:mt-12 items-center flex justify-end gap-2">
-                    <span class="cursor-pointer"></span>
+                <div class="h-10 max-md:mt-2 items-center flex justify-end gap-2">
                     <span class="cursor-pointer">More Option</span>
                 </div>
             </div>
@@ -67,13 +66,9 @@ document.addEventListener("click", (e) => {
     }
 })
 
-const goalkepeers = players.filter((gk) => gk.position === "GK")
-const deffencers = players.filter((cb) => cb.position === "CB" || cb.position === "RB" || cb.position === "LB")
-const midfielders = players.filter((cm) => cm.position === "CM")
-const attackers = players.filter((ac) => ac.position === "RW" || ac.position === "LW" || ac.position === "ST")
-
-const starters = []; // array where the 11 players will be stored
-const bench = []; // array where changment will be stored
+const starters = JSON.parse(localStorage.getItem("starters")) || []; // array where the 11 players will be stored
+const bench = JSON.parse(localStorage.getItem("bench")) || []; // array where changment will be stored
+const team = JSON.parse(localStorage.getItem("team")) || []; // variable contains all players starters and bench
 
 // function to update list players with possible players based on the position
 function updateListPlayers(array) {
@@ -82,7 +77,7 @@ function updateListPlayers(array) {
     playersList.innerHTML = "";
     array.map((player) => {
         playersList.innerHTML += `
-            <div onclick="showPlayerDetails('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
+            <div onclick="appendPlayerToTeam('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
                 <img class="h-full" src=${player.photo} alt=${player.name}>
                 <div class="flex flex-col h-full text-[10px] justify-center">
                     <span>${player.name}</span>
