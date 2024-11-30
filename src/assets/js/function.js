f0ba1e35db0f3591c3c522b825a3e75e77152df4
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-01 00:04:31 +0100

    #17 Recreate the function of appending the new player to list players with the function of showDetails

--- src/assets/js/function.js
+++ src/assets/js/function.js
@@ -201,7 +201,7 @@ function updateListPlayers(array) {
     array.map((player) => {
         playersList.innerHTML += `
             <div onclick="appendPlayerToTeam('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
-                <img class="h-full" src=${player.photo} alt=${player.name}>
+                <img class="h-full" src=${player.photo} alt="${player.name}">
                 <div class="flex flex-col h-full text-[10px] justify-center">
                     <span>${player.name}</span>
                     <span class="text-goldColor">${player.position}</span>
@@ -719,7 +719,26 @@ window.handleCreateNewPlayer = () => {
 
         // push the new player to players list then show the new players updated
         players.push(player);
-        updateListPlayers(players)
+        
+        const playersList = document.getElementById("players-list");
+        playersList.innerHTML = "";
+        players.map((player) => {
+            playersList.innerHTML += `
+                <div onclick="showPlayerDetails('${player.id}')" class="h-12 max-md:h-10 w-full p-1 hover:bg-darkGray transition-all delay-150 ease-in-out cursor-pointer flex gap-4 items-center">
+                    <img class="h-full" src=${player.photo} alt="${player.name}">
+                    <div class="flex flex-col h-full text-[10px] justify-center">
+                        <span>${player.name}</span>
+                        <span class="text-goldColor">${player.position}</span>
+                    </div>
+                    <div class="flex-1 h-full flex items-center gap-2 justify-end">
+                        <img src=${player.logo} class="h-1/2" alt="">
+                        <img src=${player.flag} class="h-1/2" alt="">
+                        <span class="text-goldColor text-lg">${player.rating}</span>
+                    </div>
+                </div>
+            `;
+        });
+
         hideFormPopUp();
     }
 
