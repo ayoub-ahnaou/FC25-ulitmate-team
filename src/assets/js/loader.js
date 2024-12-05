34e39c488e3dd4fc580b432a18f5e2551f94abe6
ayoub-ahnaou <ayoubahnaou@gmail.com>
2024-12-05 16:22:28 +0100

    #19 Add the function of toggling between the dark and light mood saving also the mood chossen in the localStorage for better UX

--- src/assets/js/loader.js
+++ src/assets/js/loader.js
@@ -1,3 +1,22 @@
 setTimeout(() => {
     document.querySelector(".loader").style.display = "none";
 }, 2000);
+
+const sun = document.getElementById("moon-icon");
+const moon = document.getElementById("sun-icon");
+const body = document.querySelector("body");
+
+let mood = localStorage.getItem("mood");
+
+if(mood == "light") body.classList.remove("dark");
+if(mood == "dark") body.classList.add("dark");
+
+sun.addEventListener("click", () => {
+    body.classList.remove("dark");
+    localStorage.setItem("mood", "light");
+});
+
+moon.addEventListener("click", () => {
+    body.classList.add("dark");
+    localStorage.setItem("mood", "dark");
+})
